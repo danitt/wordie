31ecaed7a55bd3aa6898bdbbbb7fa420a8fac57e
Dan <dan@danitt.com>
2020-09-20 16:18:46 +1000

    failed words FS utils

--- src/utils/wordlist.test.ts
+++ src/utils/wordlist.test.ts
@@ -1,7 +1,7 @@
 import { assert } from "https://deno.land/std@0.70.0/testing/asserts.ts";
 import { existsSync } from "https://deno.land/std/fs/mod.ts";
 import { writeTextFile } from "./fs.ts";
-import { getWordList } from "./wordlist.ts";
+import { getFailedWords, getWordList } from "./wordlist.ts";
 
 Deno.test("Reads wordlist", async () => {
   // Create fake words.csv to test with
@@ -16,4 +16,23 @@ Deno.test("Reads wordlist", async () => {
 
   assert(Array.isArray(words));
   assert(testWords.every(w => words.includes(w)));
+});
+
+Deno.test("Reads failed words", async () => {
+  const testFailedWordsPath = "./output/test/failed.csv";
+  const testFailedWords = ["cheesecake", "philology", "ragamuffin"];
+
+  // If not failed word list exists, should return empty string []
+  existsSync(testFailedWordsPath) && Deno.removeSync(testFailedWordsPath);
+  const failedWordsEmpty = await getFailedWords(testFailedWordsPath);
+  assert(Array.isArray(failedWordsEmpty) && failedWordsEmpty.length === 0);
+
+  // Populate failed words list, ensure returns correctly
+  const testFailedWordlist = testFailedWords.join("\n");
+  await writeTextFile(testFailedWordsPath, testFailedWordlist);
+
+  // Check that test content is parsed to string array
+  const failedWords = await getFailedWords(testFailedWordsPath);
+  assert(Array.isArray(failedWords));
+  assert(testFailedWords.every((w) => failedWords.includes(w)));
 });
--- src/utils/wordlist.ts
+++ src/utils/wordlist.ts
@@ -1,4 +1,5 @@
 import { parse } from "https://deno.land/std/encoding/csv.ts";
+import { existsSync } from "https://deno.land/std/fs/mod.ts";
 import { readTextFile } from "./fs.ts";
 
 /**
@@ -18,6 +19,31 @@ export async function getWordList(wordlistPath = './words.csv'): Promise<string[
     return words;
   } catch (e) {
     console.error('Could not read word list - please check valid words.csv file exists at project route');
+    throw (e);
+  }
+}
+
+/**
+ * Get failed word list if exists (./output/failed.csv), else return empty string[]
+ */
+export async function getFailedWords(failedWordsPath = "./output/failed.csv"): Promise<string[]> {
+  try {
+    if (!existsSync(failedWordsPath)) {
+      // No existing failed words exist, just return empty string array
+      return [];
+    }
+    const wordFileStr = await readTextFile(failedWordsPath);
+    const wordlistParsed = await parse(wordFileStr, {
+      header: false,
+    }) as string[][];
+    const words: string[] = wordlistParsed
+      // Extract first value from every line
+      .map((ln) => ln?.[0] ?? "")
+      // Remove any empty values
+      .filter((w) => !!w);
+    return words;
+  } catch (e) {
+    console.error("Could not read failed word list");
     throw (e);
   }
 }
